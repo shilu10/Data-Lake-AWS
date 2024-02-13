92233e4a10302b11bee9fc7d483b7f2465eeedb9
adminuser <adminuser@kali.mvm>
2024-02-13 20:13:24 +0530

    refactor(infra): refactored the terraform infra code

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -25,22 +25,26 @@ output "crm_db_status" {
 #####################
 
 output "saas_db_address" {
-    value = module.crm.address
+    value = module.saas.address
 }
 
 output "saas_db_arn" {
-    value = module.crm.arn
+    value = module.saas.arn
 }
 
 output "saas_db_endpoint" {
-    value = module.crm.endpoint
+    value = module.saas.endpoint
 }
 
 output "saas_db_status" {
-    value = module.crm.status
+    value = module.saas.status
 }
 
-## EC2 outputs
+
+######################
+## EC2 
+######################
+
 output "ec2_arn" {
 	value = aws_instance.this.arn
 }
